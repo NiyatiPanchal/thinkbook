08d9d63fe00cf63445eb7f436c93a0271adb4c9a
Niyati Panchal <niyatipanchal85@gmail.com>
2022-06-28 20:26:15 +0530

    updated frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { Home } from "./components/Home";
+import Navbar from "./components/Navbar";
+import { About } from "./components/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Notes App</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
